952ed32fef77e495d62c3b5451cbb87dc17be3c3
Mariusz Pawlowski <mario@data.pl>
2015-01-13 14:41:40 +0100

    initial version of application
    created with webAppCreator.cmd, empty gw t application

---
+++ Baduuum-web/src/com/baduuum/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package com.baduuum.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+  void greetServer(String input, AsyncCallback<String> callback)
+      throws IllegalArgumentException;
+}
