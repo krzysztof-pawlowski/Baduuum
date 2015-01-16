76ce26d6207fce1607fac9e9c298e3083deda008
Mariusz Pawlowski <mario@data.pl>
2015-01-16 14:31:55 +0100

    initial version of layout, not finished

---
+++ Baduuum-web/src/com/baduuum/client/BaduuumResources.java
@@ -0,0 +1,22 @@
+package com.baduuum.client;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+public interface BaduuumResources extends ClientBundle {
+	public interface MyCss extends CssResource {
+		String blackText();
+
+		String redText();
+
+		String loginButton();
+
+		String box();
+
+		String background();
+
+	}
+
+	@Source("Baduuum.css")
+	MyCss style();
+}
