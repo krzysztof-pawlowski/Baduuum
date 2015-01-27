bb1b203a1df1c66bd3e548cdae14a72e3d29ee7a
Mariusz Pawlowski <mario@data.pl>
2015-01-27 10:12:16 +0100

    [admin] added JSF capability

---
+++ Baduuum-admin/src/main/java/pl/baduuum/HelloWorld.java
@@ -0,0 +1,13 @@
+package pl.baduuum;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "helloWorld", eager = true)
+public class HelloWorld {
+   public HelloWorld() {
+      System.out.println("HelloWorld started!");
+   }
+   public String getMessage() {
+      return "Hello World!";
+   }
+}
