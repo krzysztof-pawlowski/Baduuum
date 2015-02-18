c3d8e3ade6569e7f889d486a37287ed5ebaa2ce3
Mariusz Pawlowski <mario@data.pl>
2015-02-18 16:41:42 +0100

    [web] Page layout styles, header styles, not finished

--- Baduuum-web/src/main/java/pl/baduuum/client/view/home/HomeViewImpl.java
+++ Baduuum-web/src/main/java/pl/baduuum/client/view/home/HomeViewImpl.java
@@ -8,9 +8,10 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
-public class HomeViewImpl extends BaseView implements BaduuumView {
+public class HomeViewImpl extends Composite implements BaduuumView {
 
 	@UiField StyleResources res;
 	
@@ -37,6 +38,6 @@ public class HomeViewImpl extends BaseView implements BaduuumView {
 
 	@Override
 	public void initializeMenu() {
-		setCommands(presenter);
+		//setCommands(presenter);
 	}
 }
