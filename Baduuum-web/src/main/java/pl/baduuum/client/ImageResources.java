e18afe798b11dfe8da794caaff488153e6c3c836
Mariusz Pawlowski <mario@data.pl>
2015-02-22 18:57:21 +0100

    [web] images for gallery.

--- Baduuum-web/src/main/java/pl/baduuum/client/ImageResources.java
+++ Baduuum-web/src/main/java/pl/baduuum/client/ImageResources.java
@@ -8,4 +8,13 @@ public interface ImageResources extends ClientBundle {
 	@Source("images/logo.jpg")
 	ImageResource logo();
 	
+	@Source("images//gallery//room/DSC_3526.jpg")
+	ImageResource homePage1();
+	
+	@Source("images/gallery/room/DSC_1651.jpg")
+	ImageResource homePage2();
+	
+	@Source("images/gallery/room/DSC_3022.jpg")
+	ImageResource homePage3(); 
+	
 }
