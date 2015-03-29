cd512eccd8ef3a6dbd2149c73fd87dc74ea64403
Krzysztof Pawlowski <krzysztof.pawlowski@gmail.com>
2015-03-29 08:46:14 +0200

    [admin] added login form div

---
+++ Baduuum-admin/src/main/resources/web/js/admin_app.js
@@ -0,0 +1,76 @@
+/**
+ * Created by krzysztof-pawlowski on 28/03/15.
+ */
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+$("#myModal").modal('show');
+
+(function AdminViewModel() {
+
+    var that = this;
+    var eb = new vertx.EventBus(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/eventbus');
+    that.items = ko.observableArray([]);
+
+    //eb.onopen = function() {
+    //
+    //    // Get the static data
+    //
+    //    eb.send('vertx.mongopersistor', {action: 'find', collection: 'albums', matcher: {} },
+    //        function(reply) {
+    //            if (reply.status === 'ok') {
+    //                var albumArray = [];
+    //                for (var i = 0; i < reply.results.length; i++) {
+    //                    albumArray[i] = new Album(reply.results[i]);
+    //                }
+    //                that.albums = ko.observableArray(albumArray);
+    //                ko.applyBindings(that);
+    //            } else {
+    //                console.error('Failed to retrieve albums: ' + reply.message);
+    //            }
+    //        });
+    //};
+    //
+    //eb.onclose = function() {
+    //    eb = null;
+    //};
+
+    that.username = ko.observable('');
+    that.password = ko.observable('');
+    that.loggedIn = ko.observable(false);
+
+    ko.applyBindings(that);
+
+
+    that.login = function() {
+        if (that.username().trim() != '' && that.password().trim() != '') {
+            eb.login(that.username(), that.password(), function (reply) {
+                if (reply.status === 'ok') {
+                    that.loggedIn(true);
+                    setCookie(
+                        sessionId, reply.sessionID, 1
+                    )
+                } else {
+                    alert('invalid login');
+                }
+            });
+        }
+    }
+
+})();
